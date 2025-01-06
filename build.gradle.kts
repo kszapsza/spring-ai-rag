efe383d98c1c9ecd59ea47c61a59110cddd60bd2
Karol Szapsza <kszapsza@gmail.com>
2025-01-06 14:18:50 +0100

    add Prometheus endpoint (Spring AI metrics)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,11 @@ repositories {
 dependencies {
     implementation(platform(libs.spring.ai.bom))
 
+    implementation(libs.micrometer.registry.prometheus)
     implementation(libs.postgresql)
     implementation(libs.spring.ai.openai.spring.boot.starter)
     implementation(libs.spring.ai.pgvector.store.spring.boot.starter)
+    implementation(libs.spring.boot.starter.actuator)
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.validation)
     implementation(libs.spring.boot.starter.web)
