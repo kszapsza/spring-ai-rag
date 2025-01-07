6e04f9775e5712b726cbe5ae3b7c231be9ce9431
Bartosz Galek <bartosz@galek.com.pl>
2025-01-07 12:16:52 +0100

    removed spring dependency plugin

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     java
     alias(libs.plugins.spring.boot)
-    alias(libs.plugins.spring.boot.dependency.management)
     alias(libs.plugins.integration.test)
     alias(libs.plugins.test.logger)
 }
@@ -11,7 +10,8 @@ version = "0.0.1-SNAPSHOT"
 
 dependencies {
     implementation(platform(libs.spring.ai.bom))
-    implementation(libs.micrometer.registry.prometheus)
+    implementation(platform(libs.spring.bom))
+    runtimeOnly(libs.micrometer.registry.prometheus)
     implementation(libs.postgresql)
     implementation(libs.spring.ai.openai.spring.boot.starter)
     implementation(libs.spring.ai.pgvector.store.spring.boot.starter)
